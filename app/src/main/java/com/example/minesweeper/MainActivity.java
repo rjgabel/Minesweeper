b198da659b48a338fb88b909ebb0811fe10523c5
Ryan Gabel <rjgabel@usc.edu>
2023-08-28 16:13:05 -0700

    Implemented flag counter

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -11,18 +11,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.gridlayout.widget.GridLayout;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String FLAG_TEXT = "\uD83D\uDEA9 ";
+    private static final String DIG_ICON = "⛏";
+    private static final String MINE_ICON = "\uD83D\uDCA3";
+    private static final String FLAG_ICON = "\uD83D\uDEA9";
+    private static final String FLAG_TEXT = FLAG_ICON + " ";
     private static final String TIME_TEXT = "\uD83D\uDD53 ";
+
+    private ArrayList<TextView> cell_tvs;
+    private TextView tv_flag;
+    private TextView bottom_icon;
+
     private static final int COLUMN_COUNT = 10;
     private static final int ROW_COUNT = 12;
     private static final int NUM_MINES = 4;
 
-    // save the TextViews of all cells in an array, so later on,
-    // when a TextView is clicked, we know which cell it is
-    private ArrayList<TextView> cell_tvs;
+    private final int[][] board = new int[ROW_COUNT][COLUMN_COUNT];
+    private final int[][] status = new int[ROW_COUNT][COLUMN_COUNT];
+    private boolean is_flag_mode = false;
+    private int mines_left = NUM_MINES;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -36,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
             for (int j = 0; j < COLUMN_COUNT; j++) {
                 TextView tv = (TextView) li.inflate(R.layout.custom_cell_layout, grid, false);
                 tv.setTextColor(Color.GRAY);
-                tv.setBackgroundColor(Color.GRAY);
+                tv.setBackgroundColor(Color.GREEN);
                 tv.setOnClickListener(this::onClickTV);
                 GridLayout.LayoutParams lp = (GridLayout.LayoutParams) tv.getLayoutParams();
                 lp.rowSpec = GridLayout.spec(i);
@@ -45,10 +54,23 @@ public class MainActivity extends AppCompatActivity {
                 cell_tvs.add(tv);
             }
         }
-        TextView tv_flag = findViewById(R.id.tv_flag);
+
+        tv_flag = findViewById(R.id.tv_flag);
         TextView tv_time = findViewById(R.id.tv_time);
+        bottom_icon = findViewById(R.id.bottom_icon);
         tv_flag.setText(FLAG_TEXT + NUM_MINES);
         tv_time.setText(TIME_TEXT + NUM_MINES);
+        bottom_icon.setText(DIG_ICON);
+        bottom_icon.setOnClickListener(this::switch_mode);
+
+        for (int i = 0; i < ROW_COUNT; i++) {
+            for (int j = 0; j < COLUMN_COUNT; j++) {
+                board[i][j] = 0;
+                status[i][j] = 0;
+            }
+        }
+
+        place_mines();
     }
 
     private int findIndexOfCellTextView(TextView tv) {
@@ -64,13 +86,83 @@ public class MainActivity extends AppCompatActivity {
         int n = findIndexOfCellTextView(tv);
         int i = n / COLUMN_COUNT;
         int j = n % COLUMN_COUNT;
-        tv.setText(i + String.valueOf(j));
-        if (tv.getCurrentTextColor() == Color.GRAY) {
-            tv.setTextColor(Color.GREEN);
-            tv.setBackgroundColor(Color.parseColor("lime"));
+        if (is_flag_mode) {
+            flag_cell(i, j);
         } else {
-            tv.setTextColor(Color.GRAY);
+            uncover_cell(i, j);
+        }
+    }
+
+    private void place_mines() {
+        Random rand = new Random();
+        int mines_to_place = NUM_MINES;
+        while (mines_to_place != 0) {
+            int i = rand.nextInt(ROW_COUNT);
+            int j = rand.nextInt(COLUMN_COUNT);
+            if (board[i][j] != -1) {
+                board[i][j] = -1;
+                inc_count(i - 1, j - 1);
+                inc_count(i - 1, j);
+                inc_count(i - 1, j + 1);
+                inc_count(i, j - 1);
+                inc_count(i, j + 1);
+                inc_count(i + 1, j - 1);
+                inc_count(i + 1, j);
+                inc_count(i + 1, j + 1);
+                mines_to_place--;
+            }
+        }
+    }
+
+    private void inc_count(int i, int j) {
+        if (i >= 0 && i < ROW_COUNT && j >= 0 && j < COLUMN_COUNT && board[i][j] != -1) {
+            board[i][j]++;
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void flag_cell(int i, int j) {
+        int state = status[i][j];
+        if (state != 1) {
+            TextView tv = cell_tvs.get(i * COLUMN_COUNT + j);
+            if (state == 0) {
+                status[i][j] = 2;
+                tv.setText(FLAG_ICON);
+                mines_left--;
+            } else {
+                status[i][j] = 0;
+                tv.setText("");
+                mines_left++;
+            }
+            tv_flag.setText(FLAG_TEXT + mines_left);
+        }
+    }
+
+    private void uncover_cell(int i, int j) {
+        if (i >= 0 && i < ROW_COUNT && j >= 0 && j < COLUMN_COUNT && status[i][j] == 0) {
+            status[i][j] = 1;
+            int cell = board[i][j];
+            TextView tv = cell_tvs.get(i * COLUMN_COUNT + j);
+            if (cell == 0) {
+                uncover_cell(i - 1, j - 1);
+                uncover_cell(i - 1, j);
+                uncover_cell(i - 1, j + 1);
+                uncover_cell(i, j - 1);
+                uncover_cell(i, j + 1);
+                uncover_cell(i + 1, j - 1);
+                uncover_cell(i + 1, j);
+                uncover_cell(i + 1, j + 1);
+            } else if (cell == -1) {
+                tv.setText(MINE_ICON);
+            } else {
+                tv.setText(String.valueOf(cell));
+            }
             tv.setBackgroundColor(Color.LTGRAY);
         }
+    }
+
+    public void switch_mode(View view) {
+        is_flag_mode = !is_flag_mode;
+        bottom_icon.setText(is_flag_mode ? FLAG_ICON : DIG_ICON);
     }
 }
